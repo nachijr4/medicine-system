b682f3ac6eacd823934e49ef37c1ec93fa9d6a84
Nachimuthu <nachi2muthu13@gmail.com>
2019-03-16 11:04:15 +0530

    Uploaded version

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var indexroutes = require("./routes/index.js");
 var medicine = require("./models/medicine.js")
     user     = require("./models/user.js");
 //--------------------------------------------------------------
-mongoose.connect("mongodb://nachijr4:PAssword00!!@ds113871.mlab.com:13871/medsys",{useNewUrlParser: true});
-// mongoose.connect("mongodb://localhost:27017/medsys",{useNewUrlParser: true});
+// mongoose.connect("mongodb://nachijr4:PAssword00!!@ds113871.mlab.com:13871/medsys",{useNewUrlParser: true});
+mongoose.connect(process.env.database,{useNewUrlParser: true});
+// mongoose.connect("mongodb://localhost2:27017/medsys",{useNewUrlParser: true});
 mongoose.set('useCreateIndex', true);
 console.log(__dirname);
 app.use(express.static(__dirname +"/public"));
@@ -46,50 +47,4 @@ app.use(function(req,res,next){
 app.use(indexroutes);
 app.use(medicalshopUser);
 app.use(medicalshopCustomer);
-app.listen(3000, console.log("Medsystem has started in port 3000"));
-
-
-
-
-//--------------------------------------------------------------
-  // app.get("/addmedicine",function(req,res){
-  //   res.render("addmedicine.ejs");
-  // });
-  //
-  // app.post("/addmedicine",function(req,res){
-  //   var data = {name: req.body.name, image: req.body.image, description: req.body.description}
-  //   console.log(data);
-  //   medicine.create(data, function(err,newmed){
-  //     if(err){console.log(err);}
-  //     else{
-  //       console.log(newmed);
-  //       res.redirect("/addmedicine");
-  //     }
-  //   });
-  // });
-  //
-  // app.get("/adduser",function(req,res){
-  //   res.render("adduser.ejs");
-  // });
-  //
-  // app.post("/adduser",function(req,res){
-  //   console.log(req.body);
-  //   var newUser = new user({username: req.body.username});
-  //   user.register(newUser, req.body.password, function(err,newUser){
-  //     console.log("added user");
-  //     if(err){console.log(err);}
-  //     else{
-  //         // var authenticate = passport.authenticate("local");
-  //         // authenticate(req,res,function(){
-  //         //   console.log("found user");
-  //           // user.find({username:req.body.username}, function(err,nuser){
-  //               newUser.image= req.body.image;
-  //               newUser.location= req.body.location;
-  //               newUser.save();
-  //               res.redirect("/adduser");
-  //
-  //     // });
-  //   }
-  //   });
-  // });
-
+app.listen(process.env.PORT);
